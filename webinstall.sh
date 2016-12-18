afbe4af819ef6aba95d393957f56142b69a2e34e
John Espenhahn <JohnEspenhahn@users.noreply.github.com>
2016-12-18 22:09:01 +0000

    Create webinstall.sh

---
+++ webinstall.sh
@@ -0,0 +1,2 @@
+curl -O https://raw.githubusercontent.com/JohnEspenhahn/MyLinuxInstall/master/package_check.sh
+curl -sL https://raw.githubusercontent.com/JohnEspenhahn/MyLinuxInstall/master/install.sh | sudo -E bash -
